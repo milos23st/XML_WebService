b83a888e3aa140547b46de472f1c584e0d6c9e42
milos23st <milos23stefanovic@gmail.com>
2018-08-29 23:43:52 +0200

    ..

--- frontend-client/src/app/components/user/user.ts
+++ frontend-client/src/app/components/user/user.ts
@@ -4,8 +4,8 @@ export class User {
     public prezime: string,
     public email: string,
     public password: string,
-    public phoneNumber: string,
-    public address: string,
+    public telefon: string,
+    public adresa: string,
     public id?: string
   ) { }
 }
